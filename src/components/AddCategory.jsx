f0f54b21a2feb5ad22a0b404b7aa68585f19ba90
ramiro-luna <104644012+ramiro-luna@users.noreply.github.com>
2025-06-01 16:49:26 -0300

    Primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+
+export const AddCategory = ( { onNewCategory }) => {
+  
+    const [ inputValue, setInputValue ] = useState( '' );
+
+    const onInputChange = ( { target } ) => {
+        // console.log( target.value );
+        setInputValue( target.value );
+    }
+
+    const onFormSubmit = ( event ) => {
+        event.preventDefault();
+        const newInputValue = inputValue.trim();
+        if( newInputValue.length <= 1 ) return;
+
+        //setCategories( categories =>[ inputValue, ...categories ] );
+        onNewCategory( newInputValue );
+        setInputValue('');
+    }
+  
+    return (
+        <form onSubmit={ onFormSubmit }>
+            <input 
+                type="text"
+                placeholder="Buscar gifs" 
+                value={ inputValue }
+                onChange={ onInputChange }
+            />
+        </form>
+  )
+}
---
+++ src/components/GifItem.jsx
@@ -0,0 +1,10 @@
+
+
+export const GifItem = ({ title, url, id }) => {
+  return (
+    <div className="card" key={ id }>
+        <img src={ url } alt={ title } />
+        <p>{ title }</p>
+    </div>
+  )
+}
